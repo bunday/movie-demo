416be3be04d4a3f4f812f25705daf0b78897ad09
Zadat Olayinka <bundayy@outlook.com>
2020-02-19 15:34:50 +0100

    create mvovie endpoint + form validation

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,11 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use App\Movie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\ResponseController;
 
 class MovieController extends Controller
 {
+    private function validateData(){
+        $status = true;
+        $validator = Validator::make(request()->all(),[
+            'title' => 'required',
+            'description' => 'required',
+            'release_date' => 'required',
+            'rating' => 'required|integer|between:1,5',
+            'price' => 'required|min:0',
+            'country' => 'required',
+            'genre' => 'required',
+            'image' => 'required'
+        ]);
+        if ($validator->fails()) {
+            $status = $validator->messages();
+        }
+        return $status;
+    }
     /**
      * Display a listing of the resource.
      *
@@ -14,17 +34,7 @@ class MovieController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return Movie::collection();
     }
 
     /**
@@ -35,7 +45,29 @@ class MovieController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validity = $this->validateData();
+        if ($validity=="true"){
+            $data = $request->all();
+
+            //Move the image somewhere if it exist
+            $image = $request->image;
+            $tag = time();
+            $location = '/images/films/default.png';
+            if(!is_null($image)){
+                $filename =  $tag. '.' . $image->getClientOriginalExtension();
+                $image->move(public_path('/images/films/'), $filename);
+                $location = url('/').'/images/films/' . $filename;
+            }
+
+            //Add User ID and the image to the data
+            $data['user_id'] = Auth::id();
+            $data['image'] = $location;
+            //Save to DB
+            Movie::create($data);
+            //return success
+        }
+        //return error response
+        return $this->formError($validity);
     }
 
     /**
@@ -49,37 +81,7 @@ class MovieController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Movie  $movie
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Movie $movie)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Movie  $movie
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Movie $movie)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Movie  $movie
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Movie $movie)
-    {
-        //
+    public function formError($message){
+        return response()->json(['status'=>'fail','message'=>'Oops! looks like there is an error in the Uploaded form','data'=>$message],400);
     }
 }
