dd385dd63a037348af298b433306f506fd04b46b
Zadat Olayinka <bundayy@outlook.com>
2020-02-19 21:02:39 +0100

    temporary log out

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,19 @@
 </head>
 <body>
     <div id="app">
+        <div class="">
+            <a
+              href="{{ route('logout') }}"
+              onclick="event.preventDefault();
+            document.getElementById('logout-form').submit();"
+            > Logout </a>
+            <form
+              id="logout-form"
+              action="{{ route('logout') }}"
+              method="POST"
+              style="display: none;"
+            >@csrf</form>
+          </div>
 
         <main class="h-screen">
             @yield('content')
