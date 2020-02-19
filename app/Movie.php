30c74ed39af18c13e3a80e11e9c634609c09035f
Zadat Olayinka <bundayy@outlook.com>
2020-02-19 14:32:49 +0100

    movie sturcture

---
+++ app/Movie.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    protected $fillable = ['title','description','release_date','rating','price','country','genre','image','user_id'];
+
+}
