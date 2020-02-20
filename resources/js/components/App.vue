012c76782aa2213896585b2b66eb39b793d05428
Zadat Olayinka <bundayy@outlook.com>
2020-02-20 08:24:36 +0100

    layout fix

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -8,7 +8,7 @@
         </div>
       </div>
       <div class="flex w-full px-16 py-4">
-        <router-view class="p-6 overflow-x-hidden"></router-view>
+        <router-view class="p-6 overflow-x-hidden w-full"></router-view>
       </div>
     </div>
   </div>
