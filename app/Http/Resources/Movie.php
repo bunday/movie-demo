3434a82aa80d0f4f0117e31225444cc349cb08b8
Zadat Olayinka <bundayy@outlook.com>
2020-02-20 10:14:20 +0100

    add id for easy comment

--- app/Http/Resources/Movie.php
+++ app/Http/Resources/Movie.php
@@ -15,6 +15,7 @@ class Movie extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
             'description' => $this->description,
             'rating' => $this->rating,
